ce8923338bb4e229e8537990cab5ae3508f6b704
forever <stepavoloshyn27@gmail.com>
2024-10-02 18:55:00 +0300

    buy.cpp

--- Buy.cpp
+++ Buy.cpp
@@ -2,31 +2,39 @@
 
 Buy::Buy()
 {
+    amount = 0;
 }
 
-Buy::Buy(Product p, int amount)
+Buy::Buy(Product p, int amount):Product(p)
 {
+    this->amount = amount;
 }
 
 void Buy::setAmount(int amount)
 {
+    this->amount = amount;
 }
 
 int Buy::getAmount() const
 {
-    return 0;
+    return amount;
 }
 
 float Buy::calcTotalPrice() const
 {
-    return 0.0f;
+    return amount * getPrice();
 }
 
 float Buy::calcTotalWeight() const
 {
-    return 0.0f;
+    return amount * getWeight();
 }
 
 void Buy::showBuy() const
 {
+    Product::showProduct();
+    cout << "Amount: "<< amount << endl;
+    cout << "-------------------------------\n";
+    cout << "Total price: " << calcTotalPrice() << endl;
+    cout << "Total weight: " << calcTotalWeight() << endl;
 }
