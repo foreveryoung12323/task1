362a31d18e204e1999c8c961100318653d3642a1
forever <stepavoloshyn27@gmail.com>
2024-10-02 19:15:22 +0300

    main

--- task1.cpp
+++ task1.cpp
@@ -2,12 +2,17 @@
 #include <iostream>
 #include "Product.h"
 #include "Buy.h"
+#include "Bill.h"
 int main()
 {
     Product a("Apple", 20.5, 2.5);
     /*a.showProduct();*/
+
     Buy b(a, 5);
     b.showBuy();
+
+    Bill d(b);
+    d.printBill();
 }
 
 
